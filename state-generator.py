bc3c7e43fc814e958ba11e6971156eef8beeda1a
q1e123 <robertnicolaesolca@gmail.com>
2022-04-03 12:35:15 +0300

    finished state-generator

--- state-generator.py
+++ state-generator.py
@@ -1,4 +1,5 @@
 import sys
+from turtle import st
 
 import cv2
 
@@ -14,20 +15,33 @@ def getMSB(number):
     msb = bitList[0]
     return msb
 
-print(getMSB(4),getMSB(8), getMSB(4), getMSB(128),getMSB(255))
+def get_states(image_path):
+    image = cv2.imread(image_path)
+    rows,cols,_ = image.shape
+    states = []
+    for i in range(rows):
+        row = []
+        for j in range(cols):
+            pixel = image[i,j]
+            r = pixel[0]
+            g = pixel[1]
+            b = pixel[2]
+            rgb = '{}{}{}'.format(getMSB(r), getMSB(g), getMSB(b))
+            row.append(rgb)
+        states.append(row)
+    return states
+
+def write_states_to_file(states,states_file_name):
+    with open(states_file_name, 'w') as states_file:
+        for row in states:
+            for rgb in row:
+                states_file.write(rgb + ' ')
 
 image_path = sys.argv[1]
-
-image = cv2.imread(image_path)
-rows,cols,_ = image.shape
-
-states = []
-
-for i in range(rows):
-    row = []
-    for j in range(cols):
-        pixel = image[i,j]
-        r = pixel[0]
-        g = pixel[1]
-        b = pixel[2]
-
+states_file_name = sys.argv[2]
+print('Getting states...')
+states = get_states(image_path)
+print('Done getting states!')
+print('Writing to output file...')
+write_states_to_file(states, states_file_name)
+print('Done!')
